f140168a4e2a0eccd36c56f4f559ce464c1b9a2e
Something231 <119870385+Something231@users.noreply.github.com>
2025-08-29 20:59:17 +0800

    Added hello event

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 )
 
 func main() {
+	defer fmt.Println("Program exited")
 	response, err := http.Get("https://discord.com/api/gateway")
 	if err != nil {
 		fmt.Println("Request Failed")
@@ -40,10 +41,41 @@ func main() {
 	fmt.Println(gateway.URL)
 
 	connection, response, err := websocket.DefaultDialer.Dial(gateway.URL+"/?v=10&encoding=json", nil)
-	connection.Close()
+
 	if err != nil {
 		fmt.Println("Failed to connect to gateway :(")
 		return
 	}
+	defer connection.Close()
+
 	fmt.Println("Connected to discord gateway")
+
+	var heartbeat struct {
+		OP int `json:"op"`
+		D  struct {
+			HeartbeatInterval int `json:"heartbeat_interval"`
+		} `json:"d"`
+	}
+
+	func() {
+		for {
+			_, message, err := connection.ReadMessage()
+			if err != nil {
+				fmt.Println("read:", err)
+			}
+
+			err = json.Unmarshal(message, &heartbeat)
+			if err != nil {
+				fmt.Println("Something has gone bad bad very bad", err)
+			} else if heartbeat.OP == 10 {
+				fmt.Println("Hello event processed")
+				return
+			}
+		}
+	}()
+	fmt.Println("Exiting")
+
+	//this is slightly inspired from gorilla/websocket example code
+	done := make(chan struct{})
+	defer close(done)
 }
